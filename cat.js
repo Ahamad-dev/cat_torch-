e377436b9e31204bdc0a5b0a79414662413da052
Ahamad-dev <shaikahamadalisha6865@gmail.com>
2025-07-28 23:30:25 +0530

    Initial commit

---
+++ cat.js
@@ -0,0 +1,13 @@
+function Switchof(){
+    document.getElementById("bulbimg").src="https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+    document.getElementById("catimg").src= "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+    document.getElementById("offcolor").style.backgroundColor= "red" ;
+    document.getElementById("oncolor").style.backgroundColor= "#cbd2d9" ;
+}
+function Switchon(){
+    document.getElementById("bulbimg").src="https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+    document.getElementById("catimg").src= "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+    document.getElementById("oncolor").style.backgroundColor= "green" ;
+    document.getElementById("offcolor").style.backgroundColor= "#cbd2d9" ;
+    
+}
