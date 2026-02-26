98881ff6ac8d457f734f8161c248c82dabebcd26
Ahamad-dev <shaikahamadalisha6865@gmail.com>
2026-02-26 16:33:07 +0530

    updating image links

--- cat.js
+++ cat.js
@@ -1,12 +1,12 @@
 function Switchof(){
-    document.getElementById("bulbimg").src="https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
-    document.getElementById("catimg").src= "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+    document.getElementById("bulbimg").src="https://res.cloudinary.com/delhuop9p/image/upload/v1772103306/bulb-go-off-img_ofnppd.png";
+    document.getElementById("catimg").src= "https://res.cloudinary.com/delhuop9p/image/upload/v1772103306/cat-eyes-img_ejyutq.png";
     document.getElementById("offcolor").style.backgroundColor= "red" ;
     document.getElementById("oncolor").style.backgroundColor= "#cbd2d9" ;
 }
 function Switchon(){
-    document.getElementById("bulbimg").src="https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
-    document.getElementById("catimg").src= "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+    document.getElementById("bulbimg").src="https://res.cloudinary.com/delhuop9p/image/upload/v1772103306/bulb-go-on-img_1_zk5qy1.png";
+    document.getElementById("catimg").src= "https://res.cloudinary.com/delhuop9p/image/upload/v1772103205/cat-img_rsqgph.png";
     document.getElementById("oncolor").style.backgroundColor= "green" ;
     document.getElementById("offcolor").style.backgroundColor= "#cbd2d9" ;
     
